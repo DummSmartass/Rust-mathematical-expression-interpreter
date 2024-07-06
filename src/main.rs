6278608deaa6f0fd525d364fea7156a210ae2313
DummSmartass <113634707+DummSmartass@users.noreply.github.com>
2024-07-07 01:39:47 +0200

    licznosći zaimplementowane, wszystkie funkcje poprzedniej wersjii zachowane, jedna wątpliwość

--- src/main.rs
+++ src/main.rs
@@ -41,19 +41,32 @@ fn div(args: &[f64]) -> Vec<f64> {
     vec![args[0] / args[1]]
 }
 
+fn getSeries(args: &[f64]) -> Vec<f64> {
+    if args.len() != 1 {
+        panic!("Function 'getSeries' expects exactly 1 argument");
+    }
+    let mut result: Vec<f64> = vec![];
+
+    for i in 1..=args[0] as i32 {
+        result.push(i as f64);
+    }
+    result
+}
+
 static FUNCTIONS: Lazy<HashMap<&'static str, BasicFunc>> = Lazy::new(|| {
     let mut map = HashMap::new();
     map.insert("sum", sum as BasicFunc);
     map.insert("dif", dif as BasicFunc);
     map.insert("mul", mul as BasicFunc);
     map.insert("div", div as BasicFunc);
+    map.insert("getSeries", getSeries as BasicFunc);
     map
 });
 
 struct CustomFunc<'a> {
     primary_fun: BasicFunc,
-    fun_variables: Vec<VariableType<'a>>,
-    list_elements: Vec<Vec<i64>>,
+    fun_variables: Vec<&'a VariableType<'a>>,
+    picked_elements: Vec<Vec<usize>>,
     prov_variable_names: Vec<&'a str>,
 }
 
@@ -88,14 +101,14 @@ impl<'a> CustomFunWVariables<'a> {
 impl<'a> CustomFunc<'a> {
     fn new(
         primary_fun: BasicFunc,
-        fun_variables: Vec<VariableType<'a>>,
-        list_elements: Vec<Vec<i64>>,
+        fun_variables: Vec<&'a VariableType<'a>>,
+        picked_elements: Vec<Vec<usize>>,
         prov_variable_names: Vec<&'a str>,
     ) -> CustomFunc<'a> {
         CustomFunc {
             primary_fun,
             fun_variables,
-            list_elements,
+            picked_elements,
             prov_variable_names,
         }
     }
@@ -103,11 +116,11 @@ impl<'a> CustomFunc<'a> {
     fn process_variables(
         &self,
         mapped_prov_variables: &HashMap<&'a str, f64>,
-        fun_variables: &[VariableType<'a>],
+        fun_variables: &[&VariableType<'a>],
     ) -> Vec<f64> {
         let mut processed_variables: Vec<f64> = Vec::new();
 
-        for fun_variable in fun_variables {
+        for (i, fun_variable) in fun_variables.iter().enumerate() {
             match fun_variable {
                 VariableType::Value(value) => processed_variables.push(*value),
                 VariableType::Variable(variable) => {
@@ -116,12 +129,42 @@ impl<'a> CustomFunc<'a> {
                     }
                 }
                 VariableType::BasicFunWVariables(b_func) => {
-                    let nested_processed_variables = self.process_variables(mapped_prov_variables, &b_func.fun_variables);
-                    processed_variables.extend((b_func.basic_func)(&nested_processed_variables));
+                    let nested_processed_variables = self.process_variables(mapped_prov_variables, &b_func.fun_variables.iter().collect::<Vec<_>>());
+                    let results = (b_func.basic_func)(&nested_processed_variables);
+
+                    if self.picked_elements.len() > i {
+                        let mut picked_results: Vec<f64> = vec![];
+
+                        for &j in &self.picked_elements[i] {
+                            if let Some(&result) = results.get(j) {
+                                picked_results.push(result);
+                            }
+                        }
+                        processed_variables.extend(picked_results);
+                    }
+                    else
+                    {
+                        processed_variables.extend(results);
+                    }
                 }
                 VariableType::CustomFunWVariables(c_func) => {
-                    let nested_processed_variables = c_func.custom_func.process_variables(mapped_prov_variables, &c_func.fun_variables);
-                    processed_variables.extend(c_func.custom_func.run(nested_processed_variables));
+                    let nested_processed_variables = self.process_variables(mapped_prov_variables, &c_func.fun_variables.iter().collect::<Vec<_>>());
+                    let results = (c_func.custom_func).run(nested_processed_variables);
+
+                    if self.picked_elements.len() > i {
+                        let mut picked_results: Vec<f64> = vec![];
+
+                        for &j in &self.picked_elements[i] {
+                            if let Some(&result) = results.get(j) {
+                                picked_results.push(result);
+                            }
+                        }
+                        processed_variables.extend(picked_results);
+                    }
+                    else
+                    {
+                        processed_variables.extend(results);
+                    }
                 }
             }
         }
@@ -145,9 +188,8 @@ impl<'a> CustomFunc<'a> {
 }
 
 fn main() {
-    let value1 = VariableType::Variable("x");
     let value2 = VariableType::Variable("y");
-    let variables1 = vec![value1, value2];
+    let variables1 = vec![&value2, &value2];
     let my_instance1 = CustomFunc::new(
         sum,
         variables1, // Pass variables1 without cloning
@@ -158,13 +200,13 @@ fn main() {
     let result = my_instance1.run(vec![1.0, 2.0]);
     println!("{:?}", result);
 
-    let value1 = VariableType::Variable("x");
+    let value1 = VariableType::BasicFunWVariables(BasicFunWVariables::new(getSeries, vec![VariableType::Value(5.0)]));
     let value2 = VariableType::Variable("y");
-    let variables2 = vec![value1, value2];
+    let variables2 = vec![&value1, &value2];
     let my_instance2 = CustomFunc::new(
         dif,
         variables2, // Pass variables2 without cloning
-        vec![],
+        vec![vec![0]],
         vec!["x", "y"],
     );
     let result = my_instance2.run(vec![1.0, 2.0]);
@@ -172,7 +214,7 @@ fn main() {
 
     let value1 = VariableType::CustomFunWVariables(CustomFunWVariables::new(&my_instance1, vec![VariableType::Variable("x"), VariableType::Variable("y")]));
     let value2 = VariableType::CustomFunWVariables(CustomFunWVariables::new(&my_instance2, vec![VariableType::Variable("x"), VariableType::Variable("y")]));
-    let variables = vec![value1, value2];
+    let variables = vec![&value1, &value2];
     let my_instance3 = CustomFunc::new(
         mul,
         variables,
@@ -184,7 +226,7 @@ fn main() {
 
     let value1 = VariableType::CustomFunWVariables(CustomFunWVariables::new(&my_instance3, vec![VariableType::Variable("x"), VariableType::Variable("y")]));
     let value2 = VariableType::Value(3.0);
-    let variables = vec![value1, value2];
+    let variables = vec![&value1, &value2];
     let my_instance4 = CustomFunc::new(
         div,
         variables,
