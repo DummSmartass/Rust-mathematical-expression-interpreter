28a0381859ff1a71adebb5ad5f2d4f27f3b1930f
DummSmartass <113634707+DummSmartass@users.noreply.github.com>
2024-07-06 22:55:42 +0200

    drop backups, embrance new direction, functional recursive meta creations, switch to references

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,69 @@
-use std::vec::Vec;
 use std::collections::HashMap;
 use once_cell::sync::Lazy;
 
-enum VariableType {
+enum VariableType<'a> {
     Value(f64),
-    Variable(String),
-    BasicFunWVariables(BasicFunWVariables),
-    CustomFunWVariables(CustomFunWVariables),
+    Variable(&'a str),
+    BasicFunWVariables(BasicFunWVariables<'a>),
+    CustomFunWVariables(CustomFunWVariables<'a>),
 }
 
 type BasicFunc = fn(&[f64]) -> Vec<f64>;
 
-fn a(args: &[f64]) -> Vec<f64> {
+fn sum(args: &[f64]) -> Vec<f64> {
     if args.len() != 2 {
-        panic!("Function 'a' expects exactly 2 arguments");
+        panic!("Function 'sum' expects exactly 2 arguments");
     }
     vec![args[0] + args[1]]
 }
 
+fn dif(args: &[f64]) -> Vec<f64> {
+    if args.len() != 2 {
+        panic!("Function 'dif' expects exactly 2 arguments");
+    }
+    vec![args[0] - args[1]]
+}
+
+fn mul(args: &[f64]) -> Vec<f64> {
+    if args.len() != 2 {
+        panic!("Function 'mul' expects exactly 2 arguments");
+    }
+    vec![args[0] * args[1]]
+}
+
+fn div(args: &[f64]) -> Vec<f64> {
+    if args.len() != 2 {
+        panic!("Function 'div' expects exactly 2 arguments");
+    }
+    if args[1] == 0.0 {
+        panic!("Division by zero is not allowed");
+    }
+    vec![args[0] / args[1]]
+}
+
 static FUNCTIONS: Lazy<HashMap<&'static str, BasicFunc>> = Lazy::new(|| {
     let mut map = HashMap::new();
-    map.insert("sum", a as BasicFunc);
+    map.insert("sum", sum as BasicFunc);
+    map.insert("dif", dif as BasicFunc);
+    map.insert("mul", mul as BasicFunc);
+    map.insert("div", div as BasicFunc);
     map
 });
 
-struct CustomFunc {
+struct CustomFunc<'a> {
     primary_fun: BasicFunc,
-    fun_variables: Vec<VariableType>,
+    fun_variables: Vec<VariableType<'a>>,
     list_elements: Vec<Vec<i64>>,
-    prov_variable_names: Vec<String>,
+    prov_variable_names: Vec<&'a str>,
 }
 
-struct BasicFunWVariables {
+struct BasicFunWVariables<'a> {
     basic_func: BasicFunc,
-    fun_variables: Vec<VariableType>,
+    fun_variables: Vec<VariableType<'a>>,
 }
 
-impl BasicFunWVariables {
-    fn new(basic_func: BasicFunc, fun_variables: Vec<VariableType>) -> Self {
+impl<'a> BasicFunWVariables<'a> {
+    fn new(basic_func: BasicFunc, fun_variables: Vec<VariableType<'a>>) -> Self {
         Self {
             basic_func,
             fun_variables,
@@ -45,13 +71,13 @@ impl BasicFunWVariables {
     }
 }
 
-struct CustomFunWVariables {
-    custom_func: CustomFunc,
-    fun_variables: Vec<VariableType>,
+struct CustomFunWVariables<'a> {
+    custom_func: &'a CustomFunc<'a>,
+    fun_variables: Vec<VariableType<'a>>,
 }
 
-impl CustomFunWVariables {
-    fn new(custom_func: CustomFunc, fun_variables: Vec<VariableType>) -> Self {
+impl<'a> CustomFunWVariables<'a> {
+    fn new(custom_func: &'a CustomFunc<'a>, fun_variables: Vec<VariableType<'a>>) -> Self {
         Self {
             custom_func,
             fun_variables,
@@ -59,13 +85,13 @@ impl CustomFunWVariables {
     }
 }
 
-impl CustomFunc {
+impl<'a> CustomFunc<'a> {
     fn new(
         primary_fun: BasicFunc,
-        fun_variables: Vec<VariableType>,
+        fun_variables: Vec<VariableType<'a>>,
         list_elements: Vec<Vec<i64>>,
-        prov_variable_names: Vec<String>,
-    ) -> CustomFunc {
+        prov_variable_names: Vec<&'a str>,
+    ) -> CustomFunc<'a> {
         CustomFunc {
             primary_fun,
             fun_variables,
@@ -74,31 +100,44 @@ impl CustomFunc {
         }
     }
 
-    fn run(&self, prov_variable_values: Vec<f64>) -> Vec<f64> {
-        let mapped_prov_variables: HashMap<String, f64> = self
-            .prov_variable_names
-            .iter()
-            .zip(prov_variable_values.iter())
-            .map(|(k, v)| (k.clone(), *v))
-            .collect();
-
+    fn process_variables(
+        &self,
+        mapped_prov_variables: &HashMap<&'a str, f64>,
+        fun_variables: &[VariableType<'a>],
+    ) -> Vec<f64> {
         let mut processed_variables: Vec<f64> = Vec::new();
 
-        for fun_variable in &self.fun_variables {
+        for fun_variable in fun_variables {
             match fun_variable {
                 VariableType::Value(value) => processed_variables.push(*value),
                 VariableType::Variable(variable) => {
-                    let value = mapped_prov_variables.get(variable).unwrap();
-                    processed_variables.push(*value);
+                    if let Some(value) = mapped_prov_variables.get(variable) {
+                        processed_variables.push(*value);
+                    }
                 }
                 VariableType::BasicFunWVariables(b_func) => {
-                    // Handle BasicFunWVariables here if necessary
+                    let nested_processed_variables = self.process_variables(mapped_prov_variables, &b_func.fun_variables);
+                    processed_variables.extend((b_func.basic_func)(&nested_processed_variables));
                 }
                 VariableType::CustomFunWVariables(c_func) => {
-                    // Handle CustomFunWVariables here if necessary
+                    let nested_processed_variables = c_func.custom_func.process_variables(mapped_prov_variables, &c_func.fun_variables);
+                    processed_variables.extend(c_func.custom_func.run(nested_processed_variables));
                 }
             }
         }
+
+        processed_variables
+    }
+
+    fn run(&self, prov_variable_values: Vec<f64>) -> Vec<f64> {
+        let mapped_prov_variables: HashMap<&'a str, f64> = self
+            .prov_variable_names
+            .iter()
+            .zip(prov_variable_values.iter())
+            .map(|(&k, &v)| (k, v))
+            .collect();
+
+        let processed_variables = self.process_variables(&mapped_prov_variables, &self.fun_variables);
 
         println!("Processed variables: {:?}", processed_variables);
         (self.primary_fun)(&processed_variables)
@@ -106,22 +145,52 @@ impl CustomFunc {
 }
 
 fn main() {
-    // Define some variables
-    let value1 = VariableType::Value(3.14);
-    let value2 = VariableType::Variable("x".to_string());
-
-    // Create a vector of variables
-    let variables = vec![value1, value2];
-
-    // Create an instance of CustomFunc
-    let my_instance = CustomFunc::new(
-        a,
-        variables,
-        vec![vec![0], vec![0], vec![0]],
-        vec![String::from("x"), String::from("y"), String::from("z")],
+    let value1 = VariableType::Variable("x");
+    let value2 = VariableType::Variable("y");
+    let variables1 = vec![value1, value2];
+    let my_instance1 = CustomFunc::new(
+        sum,
+        variables1, // Pass variables1 without cloning
+        vec![],
+        vec!["x", "y"],
     );
 
-    // Run the instance
-    let result = my_instance.run(vec![1.0, 2.0, 3.0, 4.0]);
+    let result = my_instance1.run(vec![1.0, 2.0]);
+    println!("{:?}", result);
+
+    let value1 = VariableType::Variable("x");
+    let value2 = VariableType::Variable("y");
+    let variables2 = vec![value1, value2];
+    let my_instance2 = CustomFunc::new(
+        dif,
+        variables2, // Pass variables2 without cloning
+        vec![],
+        vec!["x", "y"],
+    );
+    let result = my_instance2.run(vec![1.0, 2.0]);
+    println!("{:?}", result);
+
+    let value1 = VariableType::CustomFunWVariables(CustomFunWVariables::new(&my_instance1, vec![VariableType::Variable("x"), VariableType::Variable("y")]));
+    let value2 = VariableType::CustomFunWVariables(CustomFunWVariables::new(&my_instance2, vec![VariableType::Variable("x"), VariableType::Variable("y")]));
+    let variables = vec![value1, value2];
+    let my_instance3 = CustomFunc::new(
+        mul,
+        variables,
+        vec![],
+        vec!["x", "y"],
+    );
+    let result = my_instance3.run(vec![1.0, 2.0]);
+    println!("{:?}", result);
+
+    let value1 = VariableType::CustomFunWVariables(CustomFunWVariables::new(&my_instance3, vec![VariableType::Variable("x"), VariableType::Variable("y")]));
+    let value2 = VariableType::Value(3.0);
+    let variables = vec![value1, value2];
+    let my_instance4 = CustomFunc::new(
+        div,
+        variables,
+        vec![],
+        vec!["x", "y"],
+    );
+    let result = my_instance4.run(vec![1.0, 2.0]);
     println!("{:?}", result);
 }
